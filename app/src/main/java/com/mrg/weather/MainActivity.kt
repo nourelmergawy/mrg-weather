9a5e007603cb7d8aa80a1604b2b39bb38e55a01c
nourelmergawy <nouralmergawy@gmail.com>
2023-07-18 16:18:30 +0300

    areaZone

--- app/src/main/java/com/mrg/weather/MainActivity.kt
+++ app/src/main/java/com/mrg/weather/MainActivity.kt
@@ -3,44 +3,69 @@ package com.mrg.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mrg.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun areaZone() {
+        Row {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherTheme {
-        Greeting("Android")
+        }
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(
+                text = "Area",
+                fontSize = 32.sp,
+                color = Color(0xfff2f2f2),
+
+            )
+            Text(
+                text = "day",
+                fontSize = 18.sp,
+                color = Color(0xfff2f2f2),
+
+                )
+
+        }
+    }
+    @Composable
+    fun weatherDegree(){
+
+    }
+    @Composable
+    fun weatherCard() {
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun testView() {
+        Surface(modifier = Modifier.fillMaxSize(), color = Color(0xff1b1c1e)) {
+            areaZone()
+            weatherCard()
+
+
+        }
     }
 }
