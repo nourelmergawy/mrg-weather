5ebc4dbf4754a211a4a7df5148ebc4441e9e4e88
nourelmergawy <nouralmergawy@gmail.com>
2023-07-20 18:11:10 +0300

    WeatherData

---
+++ app/src/main/java/com/mrg/weather/utlis/weather/WeatherData.kt
@@ -0,0 +1,12 @@
+package com.mrg.weather.utlis.weather
+
+import java.time.LocalDateTime
+
+data class WeatherData(
+    val time: LocalDateTime,
+    val temperatureCelsius: Double,
+    val pressure: Double,
+    val windSpeed: Double,
+    val humidity: Double,
+    val weatherType: WeatherType
+)
