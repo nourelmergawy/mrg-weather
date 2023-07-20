3a58266100918106c1790134e9fa670597199177
nourelmergawy <nouralmergawy@gmail.com>
2023-07-20 16:23:51 +0300

    api

--- app/src/main/java/com/mrg/weather/MainActivity.kt
+++ app/src/main/java/com/mrg/weather/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mrg.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,9 +32,7 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun areaZone() {
-        Row {
 
-        }
         Column(modifier = Modifier.padding(24.dp)) {
             Text(
                 text = "Area",
@@ -51,7 +51,15 @@ class MainActivity : ComponentActivity() {
     }
     @Composable
     fun weatherDegree(){
-
+        Row(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+            Image(painter = painterResource(id = R.drawable.cloudy), contentDescription ="weather status" )
+        }
+        Column(modifier = Modifier.padding(24.dp)) {
+            Text(text = "18",
+                color = Color(0xfff2f2f2),
+                fontSize = 64.sp)
+            Text(text="Thunderstrom",color = Color(0xfff2f2f2))
+        }
     }
     @Composable
     fun weatherCard() {
@@ -63,6 +71,7 @@ class MainActivity : ComponentActivity() {
     fun testView() {
         Surface(modifier = Modifier.fillMaxSize(), color = Color(0xff1b1c1e)) {
             areaZone()
+            weatherDegree()
             weatherCard()
 
 
---
+++ app/src/main/java/com/mrg/weather/utlis/Resource.kt
@@ -0,0 +1,4 @@
+package com.mrg.weather.utlis
+
+class Resource {
+}
