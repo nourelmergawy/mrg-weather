76c0803507de69c5b624c341c07a93d2c1b1a5e4
nourelmergawy <nouralmergawy@gmail.com>
2023-07-21 03:53:32 +0300

    fix hilt error

--- app/src/main/java/com/mrg/weather/di/AppModule.kt
+++ app/src/main/java/com/mrg/weather/di/AppModule.kt
@@ -8,13 +8,14 @@ import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
     @Singleton
